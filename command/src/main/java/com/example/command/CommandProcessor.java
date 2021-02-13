5296eb06d18a98e50666cef92b5690e62d323d88
mrwersa <aghaeester@gmail.com>
2021-02-13 21:45:09 +0000

    Refactor

---
+++ command/src/main/java/com/example/command/CommandProcessor.java
@@ -0,0 +1,19 @@
+package com.example.command;
+
+import com.example.command.model.ReceivedOrder;
+import com.example.command.model.ValidatedOrder;
+import com.example.command.util.ProcessorUtil;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class CommandProcessor {
+    @Bean
+    public Function<KStream<String, ReceivedOrder>, KStream<String, ValidatedOrder>> orderProcessor() {
+        return receivedOrdersStream -> receivedOrdersStream
+                .mapValues(ProcessorUtil::validateOrder);
+    }
+}
---
+++ command/src/main/java/com/example/command/util/ProcessorUtil.java
@@ -0,0 +1,21 @@
+package com.example.command.util;
+
+import com.example.command.mapper.CommandMapper;
+import com.example.command.model.ReceivedOrder;
+import com.example.command.model.ValidatedOrder;
+import org.mapstruct.factory.Mappers;
+
+import java.time.Instant;
+
+public class ProcessorUtil {
+    private ProcessorUtil() {
+    }
+
+    public static ValidatedOrder validateOrder(ReceivedOrder orderReceivedMessage) {
+        ValidatedOrder validatedOrderMessage = Mappers.getMapper(CommandMapper.class)
+                .getValidatedOrderMessage(orderReceivedMessage);
+        // do the required validations
+        validatedOrderMessage.setTsValidated(Instant.now().toEpochMilli());
+        return validatedOrderMessage;
+    }
+}
