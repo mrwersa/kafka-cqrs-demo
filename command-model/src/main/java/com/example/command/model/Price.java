bc17f9c46b77e0027924d2979af85a5eadb8e476
saeed <aghaeester@gmail.com>
2021-02-17 11:43:05 +0000

    Refactor

---
+++ command-model/src/main/java/com/example/command/model/Price.java
@@ -0,0 +1,16 @@
+package com.example.command.model;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum Price {
+    CHEAP("cheap"),
+    AFFORDABLE("affordable"),
+    EXPENSIVE("expensive");
+
+    @JsonValue
+    public final String label;
+
+    Price(String label) {
+        this.label = label;
+    }
+}
---
+++ query-model/src/main/java/com/example/query/model/Item.java
@@ -0,0 +1,10 @@
+package com.example.query.model;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private String label;
+    private double price;
+    private int quantity;
+}
